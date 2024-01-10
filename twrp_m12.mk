23b710388e95509a6e8f93b0b439ff026ee94389
physwizz <pollockg86@gmail.com>
2024-01-10 11:59:57 +1100

    first

---
+++ twrp_m12.mk
@@ -0,0 +1,26 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+
+
+# Inherit from m12 device
+$(call inherit-product, device/samsung/m12/device.mk)
+
+PRODUCT_DEVICE := m12
+PRODUCT_NAME := twrp_m12
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-M127F
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="m12nsxx-user 13 TP1A.220624.014 M127FXXU4DWC4 release-keys"
+
+BUILD_FINGERPRINT := samsung/m12nsxx/m12:13/TP1A.220624.014/M127FXXU4DWC4:user/release-keys
